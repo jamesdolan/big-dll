ed7758777372be02756cffb1dab98f6efcc0499b
James Dolan <jdolan@gmail.com>
2018-12-20 19:52:36 -0800

    initial code generator

---
+++ generate_source.py
@@ -0,0 +1,7 @@
+
+
+for i in range(0,100):
+  f = open('src/functions%d.cpp' % i,  'w')
+  for j in range(0,1000):
+    idx = i*1000 + j
+    f.write('int function%d(int i) { return i + %d; }\n' % (idx, idx))
